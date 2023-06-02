fefa0122f5bbc22b601e7fcbfe0cdaf56a5934a9
Arpit Namdev <arpit8392@gmail.com>
2023-06-02 18:42:32 +0530

    Replace Quotes with Appropriate Code

--- app/page.tsx
+++ app/page.tsx
@@ -22,15 +22,15 @@ export default function Home() {
 
 			<section className='flex flex-col gap-y-4 px-8 pb-24 pt-16 text-center md:col-span-5 md:col-start-1 md:row-span-4 md:space-y-8 md:pl-40 md:text-left'>
 				<h1 className='text-5xl font-light uppercase leading-tight tracking-widest text-desaturatedRed md:text-7xl '>
-					WE'RE
+					WE&apos;RE
 					<span className='block font-semibold text-darkGrayishRed'>
 						COMING <span className='block'>SOON</span>
 					</span>
 				</h1>
 				<p className='pb-4 text-sm text-desaturatedRed md:max-w-md md:pb-2 md:text-base/7'>
-					Hello fellow shoppers! We're currently building our new fashion store.
-					Add your email below to stay up-to-date with announcements and our
-					launch deals.
+					Hello fellow shoppers! We&apos;re currently building our new fashion
+					store. Add your email below to stay up-to-date with announcements and
+					our launch deals.
 				</p>
 				<InputForm />
 			</section>
